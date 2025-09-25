7b79f9d5c515dc3c0379eaba8bc396412e4e213d
thomasmcgee1994-hue <thomasmcgee1994@gmail.com>
2025-09-24 22:10:33 -0500

    updates
    updates

--- Assets/Module1/PlayerStats.cs
+++ Assets/Module1/PlayerStats.cs
@@ -2,27 +2,32 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public int health = 100;
-    public int mana = 50;
-    public int experience = 0;
-    public int level = 1;
-    public int strength = 10;
-    public int agility = 10;
-    public int intelligence = 10;
-    public int stamina = 10;
-    public int luck = 5;
-    public int gold = 0;
-    public int inventorySize = 20;
-    
+  public int health = 100;
+  public int mana = 50;
+  public int experience = 0;
+  public int level = 1;
+  public int strength = 10;
+  public int agility = 10;
+  public int intelligence = 10;
+  public int stamina = 10;
+  public int luck = 5;
+  public int gold = 0;
+  public int inventorySize = 20;
+  public int Speed = 20;
+  public int FireResistance = 20;
+  public int IceResistance = 20;
+  public int PoisonResistance = 20;
+
+
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
-    {
-        
-    }
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
