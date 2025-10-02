c2a9b12cec0dfbfee473ab1276e627d503d69d81
thomasmcgee1994-hue <thomasmcgee1994@gmail.com>
2025-10-01 20:13:00 -0500

    test
    test

---
+++ Assets/player movment.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 1.0f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        HandleRawMovement();
+
+    }
+
+    private void HandleRawMovement()
+    {
+        if (Input.GetKey(KeyCode.))
+    }
+
+
+    
+
+}
